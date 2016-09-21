71ed04849949a6d2f9186a035bd109dd6cd7df1b
Dominic Steinitz <dominic@steinitz.org>
2016-09-21 10:38:14 +0100

    Before re-factoring.

--- src/Numeric/Particle.hs
+++ src/Numeric/Particle.hs
@@ -8,14 +8,17 @@ import           Data.Random hiding ( StdNormal, Normal )
 import           Control.Monad.Writer ( tell, WriterT, lift,
                                         runWriterT
                                       )
+import           Control.Monad
 import qualified Data.Vector as V
+import           Data.Vector ( Vector )
 import           Data.Bits ( shiftR )
 
-type Particles a = V.Vector a
+type Particles a = Vector a
+type Path a      = Vector a
 
 runPF
   :: MonadRandom m
-  => (Particles a -> m (V.Vector a)) -- ^ System evolution at a point
+  => (Particles a -> m (Vector a)) -- ^ System evolution at a point
   -> (Particles a -> Particles b)    -- ^ Measurement operator at a point
   -> (b -> b -> Double)              -- ^ Observation probability density function
   -> Particles a                     -- ^ Current estimate
@@ -32,6 +35,39 @@ runPF stateUpdate obsUpdate weight statePrevs obs = do
   let js = indices cumSumWeights vs
       stateTildes = V.map (stateNews V.!) js
   return stateTildes
+
+oneSmoothingPath'' :: MonadRandom m  =>
+                      Int ->
+                      (Int -> Vector (Particles a)) ->
+                      (a -> Particles a -> m a) ->
+                      Int ->
+                      m (Path a)
+oneSmoothingPath'' nParticles filterEstss ss nTimeSteps = do
+  let ys = filterEstss nTimeSteps
+  ix <- sample $ uniform 0 (nParticles - 1)
+  let xn = (V.head ys) V.! ix
+  scanMapM ss return xn (V.tail ys)
+
+oneSmoothingStep' :: MonadRandom m =>
+         (Particles a -> Particles a) ->
+         (a -> a -> Double) ->
+         a ->
+         Particles a ->
+         m a
+oneSmoothingStep' stateUpdate
+                 stateDensity
+                 smoothingSampleAtiPlus1
+                 filterSamplesAti = do it
+  where
+    it = do
+      let mus = stateUpdate filterSamplesAti
+          weights =  V.map (stateDensity smoothingSampleAtiPlus1) mus
+          cumSumWeights = V.tail $ V.scanl (+) 0 weights
+          totWeight     = V.last cumSumWeights
+      v <- sample $ uniform 0.0 totWeight
+      let ix = binarySearch cumSumWeights v
+          xnNew = filterSamplesAti V.! ix
+      return $ xnNew
 
 oneSmoothingStep :: MonadRandom m =>
          (Particles a -> Particles a) ->
@@ -55,30 +91,9 @@ oneSmoothingStep stateUpdate
       tell $ V.singleton xnNew
       return $ xnNew
 
-oneSmoothingStep' :: MonadRandom m =>
-         (Particles a -> Particles a) ->
-         (a -> a -> Double) ->
-         a ->
-         Particles a ->
-         m a
-oneSmoothingStep' stateUpdate
-                 stateDensity
-                 smoothingSampleAtiPlus1
-                 filterSamplesAti = do it
-  where
-    it = do
-      let mus = stateUpdate filterSamplesAti
-          weights =  V.map (stateDensity smoothingSampleAtiPlus1) mus
-          cumSumWeights = V.tail $ V.scanl (+) 0 weights
-          totWeight     = V.last cumSumWeights
-      v <- sample $ uniform 0.0 totWeight
-      let ix = binarySearch cumSumWeights v
-          xnNew = filterSamplesAti V.! ix
-      return $ xnNew
-
 oneSmoothingPath :: MonadRandom m =>
                     Int ->
-                    (Int -> V.Vector (Particles a)) ->
+                    (Int -> Vector (Particles a)) ->
                     (a -> Particles a -> WriterT (Particles a) m a) ->
                     Int -> m (a, Particles a)
 oneSmoothingPath nParticles filterEstss ss nTimeSteps = do
@@ -89,7 +104,7 @@ oneSmoothingPath nParticles filterEstss ss nTimeSteps = do
 
 oneSmoothingPath' :: MonadRandom m =>
                      Int ->
-                     (Int -> V.Vector (Particles a)) ->
+                     (Int -> Vector (Particles a)) ->
                      (a -> Particles a -> WriterT (Particles a) m a) ->
                      Int -> WriterT (Particles a) m a
 oneSmoothingPath' nParticles filterEstss ss nTimeSteps = do
@@ -98,11 +113,11 @@ oneSmoothingPath' nParticles filterEstss ss nTimeSteps = do
   let xn = (V.head ys) V.! ix
   V.foldM ss xn (V.tail ys)
 
-indices :: V.Vector Double -> V.Vector Double -> V.Vector Int
+indices :: Vector Double -> Vector Double -> Vector Int
 indices bs xs = V.map (binarySearch bs) xs
 
 binarySearch :: (Ord a) =>
-                V.Vector a -> a -> Int
+                Vector a -> a -> Int
 binarySearch vec x = loop 0 (V.length vec - 1)
   where
     loop !l !u
@@ -110,3 +125,12 @@ binarySearch vec x = loop 0 (V.length vec - 1)
       | otherwise = let e = vec V.! k in if x <= e then loop l k else loop (k+1) u
       where k = l + (u - l) `shiftR` 1
 
+scanMapM :: Monad m => (s -> a -> m s) -> (s -> m b) -> s -> Vector a -> m (Vector b)
+scanMapM f g !s0 !xs
+  | V.null xs = do
+    r <- g s0
+    return $ V.singleton r
+  | otherwise = do
+    s <- f s0 (V.head xs)
+    r <- g s0
+    liftM (r `V.cons`) (scanMapM f g s (V.tail xs))
