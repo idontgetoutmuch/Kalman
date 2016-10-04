390a70973791b883c347558adfdb38344a7c3dc7
Dominic Steinitz <dominic@steinitz.org>
2016-10-04 12:07:50 +0100

    A working particle smoother.

--- test/ParticleTest.hs
+++ test/ParticleTest.hs
@@ -22,12 +22,11 @@
 {-# LANGUAGE DataKinds                    #-}
 {-# LANGUAGE DeriveGeneric                #-}
 
-module Particle where
+module Particle ( main ) where
 
 import           Data.Random hiding ( StdNormal, Normal )
 import           Data.Random.Source.PureMT ( pureMT )
 import           Control.Monad.State ( evalState, replicateM )
-import qualified Control.Monad.Loops as ML
 import           Numeric.LinearAlgebra.Static
                  ( R, vector, Sym,
                    headTail, matrix, sym,
@@ -43,13 +42,23 @@ import           GHC.Generics (Generic)
 
 import           Numeric.Particle
 
-import           Debug.Trace
+import qualified Graphics.Rendering.Chart as C
+import Graphics.Rendering.Chart.Backend.Diagrams
+import Data.Colour
+import Data.Colour.Names
+import Data.Default.Class
+import Control.Lens
+
+import Diagrams.Backend.Cairo.CmdLine
+import Diagrams.Prelude hiding ( render, Renderable, trace, Vector, sample )
+import Diagrams.Backend.CmdLine
+
+import Data.Csv
+import System.IO hiding ( hGetContents )
+import Data.ByteString.Lazy ( hGetContents )
 
 nParticles :: Int
-nParticles = 100 -- 500
-
-nTimesteps :: Int
-nTimesteps = 10 -- 7
+nParticles = 1000 -- 500
 
 data SystemState a = SystemState { x1  :: a, x2  :: a }
   deriving (Show, Generic)
@@ -69,24 +78,6 @@ deltaT = 0.01
 g  = 9.81
 
 type PendulumState = R 2
-type PendulumObs = R 1
-
-pendulumSample :: MonadRandom m =>
-                  Sym 2 ->
-                  Sym 1 ->
-                  PendulumState ->
-                  m (Maybe ((PendulumState, PendulumObs), PendulumState))
-pendulumSample bigQ bigR xPrev = do
-  let x1Prev = fst $ headTail xPrev
-      x2Prev = fst $ headTail $ snd $ headTail xPrev
-  eta <- sample $ rvar (Normal 0.0 bigQ)
-  let x1= x1Prev + x2Prev * deltaT
-      x2 = x2Prev - g * (sin x1Prev) * deltaT
-      xNew = vector [x1, x2] + eta
-      x1New = fst $ headTail xNew
-  epsilon <-  sample $ rvar (Normal 0.0 bigR)
-  let yNew = vector [sin x1New] + epsilon
-  return $ Just ((xNew, yNew), xNew)
 
 stateUpdate :: Particles (SystemState Double) ->
                 Particles (SystemState Double)
@@ -137,24 +128,21 @@ bigP = sym $ diag 0.1
 initParticles :: MonadRandom m =>
                  m (Particles (SystemState Double))
 initParticles = V.replicateM nParticles $ do
-  r <- sample $ rvar (Normal m0' bigP)
+  r <- sample $ rvar (Normal m0 bigP)
   let x1 = fst $ headTail r
       x2 = fst $ headTail $ snd $ headTail r
   return $ SystemState { x1 = x1, x2 = x2}
 
-runFilter :: Int -> Vector (Particles (SystemState Double))
-runFilter nTimeSteps = evalState action (pureMT 19)
+runFilter :: Particles (SystemObs Double) -> Vector (Particles (SystemState Double))
+runFilter pendulumSamples = evalState action (pureMT 19)
   where
     action = do
       xs <- initParticles
       scanMapM
-        (runPF (stateUpdateNoisy bigQ') obsUpdate (weight f bigR'))
+        (runPF (stateUpdateNoisy bigQ) obsUpdate (weight f bigR))
         return
         xs
-        (V.fromList $ map (SystemObs . fst . headTail . snd)
-                          (take nTimeSteps pendulumSamples'))
-
-
+        pendulumSamples
 
 f :: SystemObs Double -> R 1
 f = vector . pure . y1
@@ -162,41 +150,96 @@ f = vector . pure . y1
 h :: SystemState Double -> R 2
 h u = vector [x1 u , x2 u]
 
-testFiltering :: Int -> Vector Double
-testFiltering nTimeSteps = V.map ((/ (fromIntegral nParticles)). V.sum . V.map x1)
-                                 (runFilter nTimeSteps)
+bigQ :: Sym 2
+bigQ = sym $ matrix bigQl
 
-bigQ' :: Sym 2
-bigQ' = sym $ matrix bigQl'
+qc1 :: Double
+qc1 = 0.01
 
-qc1' :: Double
-qc1' = 0.01
-
-bigQl' :: [Double]
-bigQl' = [ qc1' * deltaT^3 / 3, qc1' * deltaT^2 / 2,
-           qc1' * deltaT^2 / 2,       qc1' * deltaT
+bigQl :: [Double]
+bigQl = [ qc1 * deltaT^3 / 3, qc1 * deltaT^2 / 2,
+          qc1 * deltaT^2 / 2,       qc1 * deltaT
          ]
 
-bigR' :: Sym 1
-bigR'  = sym $ matrix [0.1]
+bigR :: Sym 1
+bigR  = sym $ matrix [0.1]
 
-m0' :: PendulumState
-m0' = vector [1.6, 0]
+m0 :: PendulumState
+m0 = vector [1.6, 0]
 
-pendulumSamples' :: [(PendulumState, PendulumObs)]
-pendulumSamples' = evalState (ML.unfoldrM (pendulumSample bigQ' bigR') m0') (pureMT 17)
-
-filterEstss :: V.Vector (Particles (SystemState Double))
-filterEstss = V.reverse $ runFilter nTimesteps
-
-testSmoothing' :: Int -> [Double]
-testSmoothing' n = V.toList $ evalState action (pureMT 23)
+testSmoothing :: Particles (SystemObs Double) -> Int -> [Double]
+testSmoothing ss n = V.toList $ evalState action (pureMT 23)
   where
     action = do
-      xss <- V.replicateM n $ oneSmoothingPath (stateUpdateNoisy bigQ') (weight h bigQ') nParticles filterEstss
+      xss <- V.replicateM n $ oneSmoothingPath (stateUpdateNoisy bigQ) (weight h bigQ) nParticles (runFilter ss)
       let yss = V.fromList $ map V.fromList $
                 transpose $
                 V.toList $ V.map (V.toList) $
                 xss
-      trace (show xss) $
-        return $ V.map (/ (fromIntegral n)) $ V.map V.sum $ V.map (V.map x1) yss
+      return $ V.map (/ (fromIntegral n)) $ V.map V.sum $ V.map (V.map x1) yss
+
+chartEstimated :: String ->
+              [(Double, Double)] ->
+              [(Double, Double)] ->
+              [(Double, Double)] ->
+              C.Renderable ()
+chartEstimated title acts obs ests = C.toRenderable layout
+  where
+
+    actuals = C.plot_lines_values .~ [acts]
+            $ C.plot_lines_style  . C.line_color .~ opaque red
+            $ C.plot_lines_title .~ "Actual Trajectory"
+            $ C.plot_lines_style  . C.line_width .~ 1.0
+            $ def
+
+    measurements = C.plot_points_values .~ obs
+                 $ C.plot_points_style  . C.point_color .~ opaque blue
+                 $ C.plot_points_title .~ "Measurements"
+                 $ def
+
+    estimas = C.plot_lines_values .~ [ests]
+            $ C.plot_lines_style  . C.line_color .~ opaque black
+            $ C.plot_lines_title .~ "Inferred Trajectory"
+            $ C.plot_lines_style  . C.line_width .~ 1.0
+            $ def
+
+    layout = C.layout_title .~ title
+           $ C.layout_plots .~ [C.toPlot actuals, C.toPlot measurements, C.toPlot estimas]
+           $ C.layout_y_axis . C.laxis_title .~ "Angle / Horizontal Displacement"
+           $ C.layout_y_axis . C.laxis_override .~ C.axisGridHide
+           $ C.layout_x_axis . C.laxis_title .~ "Time"
+           $ C.layout_x_axis . C.laxis_override .~ C.axisGridHide
+           $ def
+
+nObs :: Int
+nObs = 200
+
+diagU :: IO (Diagram Cairo)
+diagU = do
+  h <- openFile "matlabRNGs.csv" ReadMode
+  cs <- hGetContents h
+  let df = (decode NoHeader cs) :: Either String (V.Vector (Double, Double))
+  case df of
+    Left _ -> error "Whatever"
+    Right generatedSamples -> do
+      let preObs = V.take nObs $ V.map fst generatedSamples
+      let obs = V.toList preObs
+      let acts = V.toList $ V.take nObs $ V.map snd generatedSamples
+      let nus = take nObs (testSmoothing (V.map SystemObs preObs) 50)
+      denv <- defaultEnv C.vectorAlignmentFns 600 500
+      let charte = chartEstimated "Particle Smoother"
+                                  (zip [0,1..] acts)
+                                  (zip [0,1..] obs)
+                                  (zip [0,1..] nus)
+      return $ fst $ runBackend denv (C.render charte (600, 500))
+
+displayHeader :: FilePath -> Diagram B -> IO ()
+displayHeader fn =
+  mainRender ( DiagramOpts (Just 900) (Just 700) fn
+             , DiagramLoopOpts False Nothing 0
+             )
+
+main :: IO ()
+main = do
+  du <- diagU
+  displayHeader "diagrams/Smooth3.png" du
